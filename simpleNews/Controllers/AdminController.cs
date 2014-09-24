9b71ce76a8d222d0e95db1e3a34c1bfeaddcbcb9
Sergey <alexroverandom@gmail.com>
2014-09-24 11:59:57 +0300

    Add AdminController with ajax partitial views

---
+++ simpleNews/Controllers/AdminController.cs
@@ -0,0 +1,84 @@
+﻿using simpleNews.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace simpleNews.Controllers
+{
+    public class AdminController : Controller
+    {
+        private NewsContext dbNews = new NewsContext();
+        public ActionResult Index()
+        {
+            var articles = dbNews.Articles.OrderByDescending(x => x.Date).ToList();
+            return View(articles);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var article = dbNews.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("_Details", article);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var article = dbNews.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("_Edit", article);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                dbNews.Entry(article).State = EntityState.Modified;
+                dbNews.SaveChanges();
+            }
+            var articles = dbNews.Articles.OrderByDescending(x => x.Date).ToList();
+            return PartialView("_RefreshList", articles);
+        }
+
+        public ActionResult Create()
+        {
+            return PartialView("_NewArticle");
+        }
+
+        [HttpPost]
+        public ActionResult Create(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                article.Date = DateTime.Now;
+                dbNews.Articles.Add(article);
+                dbNews.SaveChanges();
+            }
+            var articles = dbNews.Articles.OrderByDescending(x => x.Date).ToList();
+            return PartialView("_RefreshList", articles);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            Article article = dbNews.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            dbNews.Articles.Remove(article);
+            dbNews.SaveChanges();
+            var articles = dbNews.Articles.OrderByDescending(x => x.Date).ToList();
+            return PartialView("_RefreshList", articles);
+        }
+    }
+}
